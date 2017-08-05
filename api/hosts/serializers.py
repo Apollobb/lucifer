063612dcc087eafbf42868adb6c29489f80e6b79
itimor <itimor@126.com>
2017-08-05 14:13:39 +0800

    update models

--- api/hosts/serializers.py
+++ api/hosts/serializers.py
@@ -10,14 +10,14 @@ class HostSerializer(serializers.ModelSerializer):
     group = serializers.SlugRelatedField(many=True, queryset=HostGroup.objects.all(), slug_field='name')
     class Meta:
         model = Host
-        fields = ['url', 'hostname', 'ip', 'other_ip', 'group', 'asset_type', 'status', 'os', 'cpu_model', 'cpu_num',
+        fields = ['url', 'id', 'hostname', 'ip', 'other_ip', 'group', 'asset_type', 'status', 'os', 'cpu_model', 'cpu_num',
                   'memory', 'disk', 'memo']
 
 
 class HostGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = HostGroup
-        fields = ['url', 'name', 'desc']
+        fields = ['url', 'id', 'name', 'desc']
 
 
 class SaltServerSerializer(serializers.ModelSerializer):
