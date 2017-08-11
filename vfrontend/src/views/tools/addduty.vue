4f95e670a8ec695cf0353d4375657d8b0d6c9926
itimor <itimor@126.com>
2017-08-11 20:01:37 +0800

    增加新建值班记录

--- vfrontend/src/views/tools/addduty.vue
+++ vfrontend/src/views/tools/addduty.vue
@@ -16,14 +16,13 @@
             </el-col>
             <el-col :span="12">
                 <el-upload
+                        class="avatar-uploader"
                         action="https://jsonplaceholder.typicode.com/posts/"
-                        :on-error="handleError"
-                        :on-success="handleSuccess"
-                        list-type="picture-card"
-                        ref="upload">
-                    <el-button size="small" type="primary">上传图片</el-button>
-                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb, <span style="color: red">if not, you will be destory!</span>
-                    </div>
+                        :show-file-list="false"
+                        :on-success="handleAvatarSuccess"
+                        :before-upload="beforeAvatarUpload">
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                 </el-upload>
             </el-col>
         </el-row>
@@ -38,6 +37,7 @@
         props: ['shiftOptions'],
         data() {
             return {
+                imageUrl: '',
                 ruleForm: {
                     username: '',
                     shift: '',
@@ -83,15 +83,45 @@
                 this.$refs[formName].resetFields();
                 this.$refs.upload.clearFiles();
             },
-            handleError(err, file, fileList) {
-                this.$message({
-                    message: file.name + '图片不符合要求，上传失败！',
-                    type: 'error'
-                });
+            handleAvatarSuccess(res, file) {
+                this.imageUrl = URL.createObjectURL(file.raw);
             },
-            handleSuccess(response, file, fileList) {
-                this.ruleForm.images.push(file.url)
+            beforeAvatarUpload(file) {
+                const isLt2M = file.size / 1024 / 1024 < 2;
+                if (!isLt2M) {
+                    this.$message.error('上传头像图片大小不能超过 2MB!');
+                }
+                return isLt2M;
             }
         }
     }
 </script>
+
+<style>
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .avatar-uploader .el-upload:hover {
+        border-color: #20a0ff;
+    }
+
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+    }
+
+    .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
+    }
+</style>
