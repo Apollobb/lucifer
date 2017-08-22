7e5c89612d5291d22009064a2b0d0ce93af8d36f
itimor <itimor@techdog.com>
2017-08-22 11:55:17 +0800

    解决表格多选和全选的问题

--- vfrontend/src/views/components/avatarUpload.vue
+++ vfrontend/src/views/components/avatarUpload.vue
@@ -1,51 +1,49 @@
 <template>
-  <div class="components-container">
-    <PanThumb :avatar='avatar'>
-      <p></p>
-      <a class="tou">不好意思，头掉了</a>
-    </PanThumb>
-    <el-button type="primary" icon="upload" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="imagecropperShow=true">修改头像
-    </el-button>
+    <div class="components-container">
+        <pan-thumb :image='image'></pan-thumb>
 
-    <ImageCropper :width="300" :height="300" url="https://httpbin.org/post" @close='close' @crop-upload-success="cropSuccess"
-      :key="imagecropperKey" v-show="imagecropperShow" />
-  </div>
+        <el-button class="upload" type="primary" icon="upload"
+                   style="position: absolute;bottom: 15px;margin-left: 40px;"
+                   @click="imagecropperShow=true">修改头像
+        </el-button>
+
+
+        <image-cropper :width="300" :height="300" url="https://httpbin.org/post" @close='close'
+                       @crop-upload-success="cropSuccess" :key="imagecropperKey"
+                       v-show="imagecropperShow"></image-cropper>
+    </div>
 </template>
 
 <script>
     import ImageCropper from 'components/ImageCropper';
     import PanThumb from 'components/PanThumb';
     import {mapGetters} from 'vuex';
+
     export default {
-      components: { ImageCropper, PanThumb },
-      data() {
-        return {
-          imagecropperShow: false,
-          imagecropperKey: 0,
-        }
-      },
-
+        components: {ImageCropper, PanThumb},
+        data() {
+            return {
+                imagecropperShow: false,
+                imagecropperKey: 0,
+                image: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191'
+            }
+        },
         computed: {
-            ...mapGetters([
-                'avatar'
-            ])
+            ...mapGetters(['avatar'])
         },
-
-      methods: {
-        cropSuccess(resData) {
-          this.imagecropperShow = false;
-          this.imagecropperKey = this.imagecropperKey + 1;
-          this.avatar = resData.files.avatar;
-        },
-        close() {
-          this.imagecropperShow = false;
+        methods: {
+            cropSuccess(resData) {
+                this.imagecropperShow = false;
+                this.imagecropperKey = this.imagecropperKey + 1;
+                this.image = resData.files.avatar;
+            },
+            close() {
+                this.imagecropperShow = false;
+            }
         }
-      }
     };
 </script>
+
 <style scoped>
-.tou {
-  color: #ff363a;
-}
 </style>
 
